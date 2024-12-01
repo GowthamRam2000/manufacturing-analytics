882d4cf781f4bd31c3dd4facf3811dd378f6d631
Gowtham Ram M <gowthamm161@gmail.com>
2024-12-01 20:56:46 +0530

    Update publisher_script.py

--- real_time_pipeline/publisher/publisher_script.py
+++ real_time_pipeline/publisher/publisher_script.py
@@ -1,25 +1,18 @@
-# publisher_script.py
 from sensor_publisher import RealTimeDataPublisher
 import time
 import logging
 from google.oauth2 import service_account
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 PROJECT_ID = 'log-project-442913'
 TOPIC_NAME = 'sensor-data-stream'
 SERVICE_ACCOUNT_PATH = '/Users/gowthamram/PycharmProjects/manufacturing_analytics/service-account.json'
-
-
 def main():
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_PATH,
         scopes=['https://www.googleapis.com/auth/pubsub']
     )
-
     publisher = RealTimeDataPublisher(PROJECT_ID, TOPIC_NAME, credentials)
-
     try:
         logger.info("Starting real-time data publication...")
         while True:
